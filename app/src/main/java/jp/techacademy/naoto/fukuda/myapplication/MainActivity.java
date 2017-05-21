64acefe93e2b36e569b46e1bfb595e60a50798e4
naotofukuda <nfuku30@gmail.com>
2017-05-21 21:55:47 +0900

    Humanクラスの定義追加。

--- app/src/main/java/jp/techacademy/naoto/fukuda/myapplication/MainActivity.java
+++ app/src/main/java/jp/techacademy/naoto/fukuda/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package jp.techacademy.naoto.fukuda.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Human human = new Human("Human", 10, "プログラミング");
+
+        human.say();
+
+        human.think();
+
+      }
+
+
+
 }
